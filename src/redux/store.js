8bc9cd0107fd766c7276f905bc78f5c14b90aaa2
Syed Muhammad Shahid <engrshahid1123@gmail.com>
2022-10-05 13:53:09 +0500

    Bug Fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
-import {createStore, combineReducer, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+
 import userReducer from './reducer';
 
-const rootReducer = combineReducer({userReducer});
+const rootReducer = combineReducers({userReducer});
 export const Store = createStore(rootReducer, applyMiddleware(thunk));
+
+
